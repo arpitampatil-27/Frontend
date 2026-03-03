396a2fda59db1d084aced63f99ab36ded79971d7
arpitampatil-27 <arpitampatil27@gmail.com>
2026-03-03 15:53:57 +0530

    Learn useEffect and useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import Dashboard from "./pages/Dashboard"
-import Login from "./pages/login"
-import Signup from "./pages/signup"
+import { useState, useEffect } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   return (
-    <>
-    <Dashboard />
-    </>
-  )
+    <div>
+      <h1>MERN Auth Demo</h1>
+
+      {isLoggedIn ? (
+        <p>You are logged in ✅</p>
+      ) : (
+        <button onClick={() => {
+          localStorage.setItem("token", "dummy");
+          setIsLoggedIn(true);
+        }}>
+          Login
+        </button>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
